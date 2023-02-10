6846dc12b09936cb2ed841ff82cdf189aa1fa322
Coderzzz1 <124791890+Coderzzz1@users.noreply.github.com>
2023-02-10 15:50:11 +0800

    cutie

---
+++ intayon/lib/registration.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Registration extends StatefulWidget {
+  const Registration({super.key});
+
+  @override
+  State<Registration> createState() => _RegistrationState();
+}
+
+class _RegistrationState extends State<Registration> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
