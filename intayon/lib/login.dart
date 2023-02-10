5849620ecaace5215433dc19cc93aaac1de34873
Coderzzz1 <124791890+Coderzzz1@users.noreply.github.com>
2023-02-10 21:29:14 +0800

    aj

--- intayon/lib/login.dart
+++ intayon/lib/login.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -11,6 +10,10 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        child: Text("Login"),
+      ),
+    );
   }
 }
