182fdcb05ede6e55926fe8d478d72b412dd56914
Coderzzz1 <124791890+Coderzzz1@users.noreply.github.com>
2023-02-10 21:27:10 +0800

    aj

--- intayon/lib/home.dart
+++ intayon/lib/home.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,6 +10,10 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        child: Text("string"),
+      ),
+    );
   }
 }
